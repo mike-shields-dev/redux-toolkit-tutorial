8215eb084ab1ae29b985c3e6d4d2fa70a00a6523
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-04-17 17:36:45 +0100

    refactor: Remove unused useSelector import from Modal comp

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,4 +1,4 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { closeModal } from "../features/modal/modalSlice";
 import { clearCart } from "../features/cart/cartSlice";
 
