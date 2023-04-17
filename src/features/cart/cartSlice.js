b7e3b780239fa478960592a2381d4fc39564741b
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-04-17 14:45:02 +0100

    chore: Add removeItem reducer to cartSlice

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -15,6 +15,14 @@ const cartSlice = createSlice({
         clearCart: state => {
             state.cartItems = [];
         },
+        removeItem: (state, action) => {
+            const itemId = action.payload 
+            
+            state.cartItems = 
+            state.cartItems.filter(item => 
+                item.id !== itemId
+            )
+        }
     }, 
 });
 
