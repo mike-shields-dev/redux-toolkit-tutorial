26bd7c6cafc9a8a3fd4626645a6cd36a3baadb17
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-04-17 15:10:29 +0100

    refactor: Format file

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,43 +1,37 @@
 import { createSlice } from "@reduxjs/toolkit";
-import cartItems from '../../cartItems';
+import cartItems from "../../cartItems";
 
 const initialState = {
-    cartItems,
-    amount: 0,
-    total: 0,
-    isLoading: true,
-}
+  cartItems,
+  amount: 1,
+  total: 0,
+  isLoading: true,
+};
 
 const cartSlice = createSlice({
-    name: 'cart',
-    initialState,
-    reducers: {
-        clearCart: state => {
-            state.cartItems = [];
-        },
-        removeItem: (state, action) => {
-            const itemId = action.payload 
-            
-            state.cartItems = 
-            state.cartItems.filter(item => 
-                item.id !== itemId
-            )
-        },
-        increase: (state, action) => {
-            const id = action.payload;
-            const cartItem = 
-            state.cartItems.find(item => item.id === id)
-            cartItem.amount += 1
-        }, 
-        decrease: (state, action) => {
-            const { id: itemId } = action.payload;
-            const cartItem = 
-            state.cartItems.find(item => item.id === itemId)
-            cartItem.amount = Math.max(1, cartItem - 1);
-        }
-    }, 
+  name: "cart",
+  initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
+    increase: (state, action) => {
+      const id = action.payload;
+      const cartItem = state.cartItems.find((item) => item.id === id);
+      cartItem.amount += 1;
+    },
+    decrease: (state, action) => {
+      const { id: itemId } = action.payload;
+      const cartItem = state.cartItems.find((item) => item.id === itemId);
+      cartItem.amount = Math.max(1, cartItem - 1);
+    },
+  },
 });
 
-export const { clearCart, removeItem, increase, decrease} = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
 export default cartSlice.reducer;
-
