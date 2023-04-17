02a9ad0b6e54add4649c4c6c483f8299fdda5304
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-04-17 15:14:10 +0100

    feat: Dispatch increase/decrease actions from increase/decrease buttons in CartItem

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,5 +1,5 @@
 import { ChevronUp, ChevronDown } from "../icons";
-import { removeItem } from "../features/cart/cartSlice";
+import { decrease, increase, removeItem } from "../features/cart/cartSlice";
 import { useDispatch } from "react-redux";
 
 const CartItem = ({ id, img, title, price, amount }) => {
@@ -20,11 +20,19 @@ const CartItem = ({ id, img, title, price, amount }) => {
         </button>
       </header>
       <div>
-        <button type="button" className="amount-btn">
+        <button
+          type="button"
+          className="amount-btn"
+          onClick={() => dispatch(increase(id))}
+        >
           <ChevronUp />
         </button>
         <p className="amount">{amount}</p>
-        <button type="button" className="amount-btn">
+        <button
+          type="button"
+          className="amount-btn"
+          onClick={() => dispatch(decrease(id))}
+        >
           <ChevronDown />
         </button>
       </div>
