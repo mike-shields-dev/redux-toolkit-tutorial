1111e669c100898e4de82c0ff3b9f23e041f790e
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-04-17 17:28:30 +0100

    chore: Modal comp confirms and cancels clearing of the cart and closes itself

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,19 +1,40 @@
-import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { closeModal } from "../features/modal/modalSlice";
+import { clearCart } from "../features/cart/cartSlice";
 
 const Modal = () => {
+  const dispatch = useDispatch();
+
+  const handleConfirm = () => {
+    dispatch(closeModal());
+    dispatch(clearCart());
+  };
+
+  const handleCancel = () => {
+    dispatch(closeModal());
+  };
+
   return (
     <div className="modal-container">
-        <div className="modal">
-            <h4>remove all items from your shopping cart?</h4>
-            <div className="btn-container">
-                <button className="btn confirm-btn" type="button">
-                confirm
-                </button>
-                <button className="btn clear-btn" type="button">
-                    cancel
-                </button>
-            </div>
+      <div className="modal">
+        <h4>remove all items from your shopping cart?</h4>
+        <div className="btn-container">
+          <button
+            className="btn confirm-btn"
+            type="button"
+            onClick={handleConfirm}
+          >
+            confirm
+          </button>
+          <button
+            className="btn clear-btn"
+            type="button"
+            onClick={handleCancel}
+          >
+            cancel
+          </button>
         </div>
+      </div>
     </div>
   );
 };
