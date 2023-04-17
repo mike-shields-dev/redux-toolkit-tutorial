9092cdbc93adabd38ffd5be958c17751fd3362b0
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-04-17 15:50:24 +0100

    chore: Create calculateTotals reducerin cartSlice

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -26,12 +26,27 @@ const cartSlice = createSlice({
       cartItem.amount += 1;
     },
     decrease: (state, action) => {
-      const id = action.payload
+      const id = action.payload;
       const cartItem = state.cartItems.find((item) => item.id === id);
       cartItem.amount = Math.max(1, cartItem.amount - 1);
+    },
+    calculateTotals: (state) => {
+      const { total, amount } = state.cartItems.reduce(
+        (reduced, item) => {
+          reduced.amount += item.amount;
+          reduced.total += (item.amount * item.price);
+
+          return reduced;
+        },
+        { amount: 0, total: 0 }
+      );
+      state.total = total;
+      state.amount = amount;
     },
   },
 });
 
-export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease, calculateTotals } =
+  cartSlice.actions;
+
 export default cartSlice.reducer;
