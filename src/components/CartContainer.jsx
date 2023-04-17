76cbb030b397167d3c9f7d55704e4a6f0f9cfb4a
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-04-17 14:39:16 +0100

    feat: Dispatch clearCart action from clear cart button

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -1,36 +1,40 @@
-import { useSelector } from 'react-redux'
-import CartItem from './CartItem'
+import { useDispatch, useSelector } from "react-redux";
+import CartItem from "./CartItem";
+import { clearCart } from "../features/cart/cartSlice";
 
 const CartContainer = () => {
-  const { cartItems, amount, total } = useSelector(store => store.cart);
-  
-    return (
-      <section className="cart">
-        <header>
-          <h2>your bag</h2>
-          {amount < 1 &&
-            <h4 className="empty-cart">is empty</h4>
-          }
-        </header>
-        
-        {amount > 0 &&
-          <div>
-            {cartItems.map(item => 
-              <CartItem key={item.id} {...item} />
-            )}
-          </div>
-        }
-        <footer>
-            <hr/>
-            <div className='cart-total'>
-              Total <span>{total}</span>
-            </div>
-            <button type="button" className="btn clear-btn">clear cart</button>
-          </footer> 
-      </section>
-    )
-}
+  const { cartItems, amount, total } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
 
+  return (
+    <section className="cart">
+      <header>
+        <h2>your bag</h2>
+        {amount < 1 && <h4 className="empty-cart">is empty</h4>}
+      </header>
 
+      {amount > 0 && (
+        <div>
+          {cartItems.map((item) => (
+            <CartItem key={item.id} {...item} />
+          ))}
+        </div>
+      )}
+      <footer>
+        <hr />
+        <div className="cart-total">
+          Total <span>{total}</span>
+        </div>
+        <button
+          type="button"
+          className="btn clear-btn"
+          onClick={() => dispatch(clearCart())}
+        >
+          clear cart
+        </button>
+      </footer>
+    </section>
+  );
+};
 
-export default CartContainer
+export default CartContainer;
