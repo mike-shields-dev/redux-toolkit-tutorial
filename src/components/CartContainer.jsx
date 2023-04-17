fff3ecf045081f8b9bb6d60c79b7e812b33082d9
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-04-17 12:26:34 +0100

    chore: Create CartContainer with CartItem as child

---
+++ src/components/CartContainer.jsx
@@ -0,0 +1,12 @@
+import CartItem from './CartItem'
+
+const CartContainer = () => {
+  return (
+    <div>
+      CartContainer
+      <CartItem />
+    </div>
+  )
+}
+
+export default CartContainer
